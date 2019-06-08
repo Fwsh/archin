46cf9e770908a41333049e3cb379a3ac8daf1b5f
Fwsh <51488168+Fwsh@users.noreply.github.com>
2019-06-08 13:17:59 -0400

    Add files via upload

--- archin.sh
+++ archin.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # archin by github.com/Fwsh
 
-pacman -Sy # Initialize pacman just in case
+read -p "Press ENTER to start the installation process."
+wget raw.githubusercontent.com/Fwsh/archin/master/archin2.sh
+wget raw.githubusercontent.com/Fwsh/archin/master/archin2.sh
 
 ## Let's create partitions.
 ## You will need a boot partition, a swap partition and a root partition.
 ## Please use "Linux Filesystem" for everything except swap (use Linux Swap)
 
+read -p "Currently, sda1 = boot, sda2 = swap, sda3 = root"
 cfdisk # 100M (Boot / Linux Filesystem) /// 1G-4G (Linux Swap) /// Rest (root / Linux Filesystem)
 ##(User creates partitons)
 
@@ -18,20 +21,23 @@ cfdisk # 100M (Boot / Linux Filesystem) /// 1G-4G (Linux Swap) /// Rest (root / 
 ##(answer)
 
 ## We will now be formatting everything you selected. Proceed?
-
+read -p "WARNING: This will format everything in sda1, sda2 and sda3. Press ENTER to proceed."
 mkfs.ext2 /dev/sda1 #boot
 mkswap /dev/sda2 #swap
 mkfs.ext4 /dev/sda3 #root
 swapon /dev/sda2 #activate swap if exists
 
 ## We will now mount the root partition.
-
+read -p "We will now mount the root partition."
 mount /dev/sda3 /mnt # Mount root on /mnt
 mount /dev/sda1 /mnt/boot # Mount boot or efi
 
 # Installing pacstrap base
-
+read -p "Press ENTER to install pacstrap base."
 pacstrap /mnt base # install the "base" package group to the mounted partition
+
+# Copy the second script to the install itself
+cp archin2.sh /mnt/archin2.sh
 
 # Generating fstab
 
@@ -39,5 +45,5 @@ genfstab -U /mnt >> /mnt/etc/fstab # Generate fstab
 
 # chrooting into the new system
 
-arch-chroot /mnt # Root into the new system
+arch-chroot /mnt ./archin2.sh # Root into the new system
 
--- archin2.sh
+++ archin2.sh
@@ -1,19 +1,19 @@
 # Setting timezone
-
+read -p "Press ENTER to set timezone."
 ln -sf /usr/share/zoneinfo/Canada/Toronto /etc/localtime #set timezone
 hwclock --systohc # generate /etc/adjtime
 
 # Please uncomment your prefered locale (e.g., uncomment en_CA.utf-8 UTF-8)
-
+read -p "Please uncomment your prefered locale (e.g., en_CA.utf-8 UTF8)"
 nano /etc/locale.gen # Uncomment #en_CA.UTF-8 UTF-8
 locale-gen # Generate what you just uncommented
 
-# Please type the locale you just chose (e.g., LANG-en_CA.UTF-8)
-
+# Please type the locale you just chose (e.g., LANG=en_CA.UTF-8)
+read -p "Please type the locale you just chose (e.g., LANG=en_CA.UTF-8)"
 nano /etc/locale.conf # Write   LANG=en_CA.UTF-8
 
 # Hostname stuff
-
+read -p "Hosts stuff, see readme"
 nano /etc/hostname # Write the computer's hostname, for example "arch"
 nano /etc/hosts # Add the matching entries to the hosts file
 # 127.0.0.1   localhost
@@ -21,20 +21,22 @@ nano /etc/hosts # Add the matching entries to the hosts file
 # 127.0.0.1   myhostname.localdomain  myhostname
 
 # Generating mkinitcpio
-
+read -p "Press ENTER to mkinitcpio"
 mkinitcpio -p linux # Create initramfs
 
 # Accounts
-
+read -p "Press ENTER to set password"
 passwd # Set the root password (archin)
 
 # Getting grub and installing it
-
+read -p "Press ENTER to get grub and efibootmgr"
 pacman -S grub efibootmgr # Install grub and efibootmgr
 
+read -p "Press enter to continue"
 mkdir /boot/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
+read -p "Press enter to continue"
 # Only either one of the following:
 grub-install --force --target=i386-pc --recheck /dev/sda
 # grub-install --target=x86_64-efi --efi-directory=/boot --recheck
@@ -52,3 +54,4 @@ grub-install --force --target=i386-pc --recheck /dev/sda
 #exit
 
 #reboot
+read -p "Finished."
