97e2d9a98f176944573538abbc6642a99cc2fcd8
Fwsh <51488168+Fwsh@users.noreply.github.com>
2019-06-08 14:41:42 -0400

    Add files via upload

--- archin.sh
+++ archin.sh
@@ -6,16 +6,18 @@ read -p "Press ENTER to start the installation process."
 wget raw.githubusercontent.com/Fwsh/archin/master/archin2.sh
 wget raw.githubusercontent.com/Fwsh/archin/master/archind.sh
 wget raw.githubusercontent.com/Fwsh/archin/master/hosts
-
+clear
 
 echo "The partition manager will now launch. Make sure you have a boot, swap and root."
 echo "You can manually choose the drive you want to use."
 read -p "Press ENTER to list your drives."
+clear
 fdisk -l
 
 
 echo "Please enter the drive you want to use. For example, /dev/sda"
 read -p "Drive: " drivetouse
+clear
 echo "We will be using the drive '$drivetouse'."
 echo "Please make sure to have a BOOT, a SWAP and a ROOT partition."
 echo "Example of sizes/filesystems:"
@@ -24,7 +26,7 @@ echo "SWAP = 4G = Linux Swap."
 echo "ROOT = 15G = Linux Filesystem.   //  You can set whatever size you want for ROOT."
 read -p "Press ENTER whenever you're ready."
 cfdisk $drivetouse
-
+clear
 
 read -p "What is your BOOT partition? e.g. /dev/sda1" bootpartition
 read -p "What is your SWAP partition? e.g. /dev/sda2" swappartition
@@ -43,11 +45,14 @@ swapon $swappartition #activate swap if exists
 
 
 read -p "We will now mount the root partition."
+clear
 mount $rootpartition /mnt
 mount $bootpartition /mnt/boot
 echo "It doesn't matter if /mnt/boot didn't mount."
 
+
 read -p "Press ENTER to install the base system."
+clear
 pacstrap /mnt base
 
 
@@ -60,7 +65,7 @@ cp hosts /mnt/hosts
 echo "Generating fstab..."
 genfstab -U /mnt >> /mnt/etc/fstab # Generate fstab
 
-
+clear
 # chrooting into the new system
 read -p "Please manually do 'arch-chroot /mnt' and 'sh archin2.sh' to continue."
 #arch-chroot /mnt ./archin2.sh # Root into the new system
--- archin2.sh
+++ archin2.sh
@@ -1,24 +1,29 @@
 # Setting timezone
+clear
 read -p "Press ENTER to set timezone."
 ln -sf /usr/share/zoneinfo/Canada/Toronto /etc/localtime #set timezone
 hwclock --systohc # generate /etc/adjtime
 
 
 # Please uncomment your prefered locale (e.g., uncomment en_CA.utf-8 UTF-8)
+clear
 read -p "Please uncomment your prefered locale (e.g., en_CA.utf-8 UTF8)"
 nano /etc/locale.gen # Uncomment #en_CA.UTF-8 UTF-8
 locale-gen # Generate what you just uncommented
 
 
 # Please type the locale you just chose (e.g., LANG=en_CA.UTF-8)
+clear
 read -p "Please type the locale you just chose (e.g., LANG=en_CA.UTF-8)"
 nano /etc/locale.conf # Write   LANG=en_CA.UTF-8
 
 
 # Hostname stuff
+clear
 read -p "Please type in your hostname (aka your computer's name)."
 nano /etc/hostname # Write the computer's hostname, for example "arch"
 cp hosts /etc/hosts
+clear
 read -p "Please replace 'arch' with the hostname you just chose."
 nano /etc/hosts # Add the matching entries to the hosts file
 # 127.0.0.1   localhost
@@ -26,14 +31,17 @@ nano /etc/hosts # Add the matching entries to the hosts file
 # 127.0.0.1   myhostname.localdomain  myhostname
 
 # Generating mkinitcpio
+clear
 echo "Generating mkinitcpio"
 mkinitcpio -p linux # Create initramfs
 
 # Accounts
+clear
 read -p "Press ENTER to set a root password."
 passwd # Set the root password (archin)
 
 # Getting grub and installing it
+clear
 read -p "Press ENTER to install grub."
 pacman -S grub efibootmgr # Install grub and efibootmgr
 
@@ -42,10 +50,12 @@ mkdir /boot/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # Only either one of the following:
+clear
 read -p "IMPORTANT - What is your boot drive? e.g. /dev/sda" bootdrive
 grub-install --force --target=i386-pc --recheck $bootdrive
 # grub-install --target=x86_64-efi --efi-directory=/boot --recheck
 
+clear
 echo "Finished."
 echo "The base system is now installed. However, you may still want to install extra tools."
 echo "If you want help with enabling your ethernet port as well as installing a"
