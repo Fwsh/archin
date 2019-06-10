94539a2cd3c60d0f351efcd7a1220c00dea217a7
Fwsh <51488168+Fwsh@users.noreply.github.com>
2019-06-10 05:48:57 -0400

    Add files via upload

--- archinb/archinbt.sh
+++ archinb/archinbt.sh
@@ -5,7 +5,7 @@ readarray -t lines < <(lsblk --nodeps -no name | grep "sd")
 echo "Please select a drive:"
 select choice in "${lines[@]}"; do
   [[ -n $choice ]] || { echo "Invalid choice. Please try again." >&2; continue; }
-  break # valid
+  break # valid, proceed
 done
 
 read -r id sn unused <<<"$choice"
